c8e2e4b5ae44fc9e93dc0572a2b0b1c41677a44c
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-09-11 14:13:00 +0300

    Master
    add new box in feed and create videos.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,15 +6,36 @@ import { Videos, Sidebar } from "./";
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-      <Sidebar/>
-      
-      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-        Copyright © 2022
-      </Typography>
-    </Box>
-  </Stack>
-  )
-}
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
 
-export default Feed
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bolt"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+        <Videos/>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
